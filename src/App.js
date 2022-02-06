3ece72776a2d13c9ce8489fb364068c83e3d7ec6
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-06 14:11:10 +0530

    eth and weth img added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import CollectionCard from './Components/CollectionCards/CollectionCard';
 
 function App() {
   return (
     <div className='app'>
       <Header/>
+      <CollectionCard/>
     </div>
   );
 }
