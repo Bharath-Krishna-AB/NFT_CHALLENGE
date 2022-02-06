9eaefc9620df39848d3ae96874ce90032916919f
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-06 14:43:54 +0530

    designed collection card but not complete

--- src/Components/CollectionCards/CollectionCard.js
+++ src/Components/CollectionCards/CollectionCard.js
@@ -5,13 +5,14 @@ const CollectionCard = ({id,name,traits,image}) =>{
   return (
       <div className="collectionCard">
           <img src={image} alt="" />
-          <div className="details"></div>
+          <div className="details">
           <div className="name">
               {name} <div className="id">#{id}</div>
           </div>
           <div className="priceContainer">
               <img src="Assets/owner/weth.png" alt="" className="weth" />
               <div className="price"></div>
+          </div>
           </div>
       </div>
   )
