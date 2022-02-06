7fee400ae05f53533b575effeba821c9eb06e5ee
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-06 14:08:04 +0530

    collection card skeleton completed

--- src/Components/CollectionCards/CollectionCard.js
+++ src/Components/CollectionCards/CollectionCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function CollectionCard() {
+const CollectionCard = ({id,name,traits,image}) =>{
   return (
       <div className="collectionCard">
-          
+          <img src={image} alt="" />
+          <div className="details"></div>
+          <div className="name">
+              {name} <div className="id">#{id}</div>
+          </div>
+          <div className="priceContainer">
+              <img src="" alt="" className="eth" />
+              <div className="price"></div>
+          </div>
       </div>
   )
+  
+}
 }
 
 export default CollectionCard;
