41b119ca798a2b13a0c0c9853b0fa4af9e350427
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-02-06 13:38:25 +0530

    added headerItems skeleton

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,15 +4,24 @@ import './Header.css'
 function Header() {
   return (
   <div className='header'>
+
       <div className="logo-container">
           <img src="Assets/header/punkLogo.png" className='punkLogo' alt="" />
       </div>
+
       <div className="searchBar">
         <div className="searchIconContainer">
           <img src="Assets/header/search.png" alt="" />
         </div>
         <input type="text" className="searchInput"  placeholder='Collection, item or user ...'/>
       </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+      
   </div>
   );
 }
